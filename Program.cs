6305a9163582b8d66fea65e33b594969b7e5bcd7
Darren Siriram <darren.siriram@cegeka.com>
2024-12-06 14:50:28 +0100

    fixed issue with starting threads in a wrong way.

--- Program.cs
+++ Program.cs
@@ -6,73 +6,56 @@ namespace pizzeria //this is useless, if you remove it your assignment will be N
 {
     internal class Program // feel free to add methods/variables to this class
     {
-        public static int n_slices = 4;
-        public static int n_customers = 1000;
-        public static int n_pizzaioli = n_customers;
+        public static int n_slices = 4; // Number of slices per pizza, 
+        // maximum amount of customers per pizza default: 4
+        public static int n_customers = 1000; // must be a multiple of n_slices, default: 1000
+        public static int n_pizzaioli = n_customers; // must be the same as n_customers
         
-        // Semaphores without upper bounds to prevent deadlocks
-        public static Semaphore order_ready = new(0, int.MaxValue);
-        public static Semaphore slice_ready = new(0, int.MaxValue);
-        
-        public static LinkedList<PizzaOrder> order = new();
-        public static LinkedList<PizzaDish> pickUp = new();
-        public static LinkedList<PizzaSlice> workingsurface = new();
-        
-        public static Pizzaiolo[] pizzaioli;
-        public static Customer[] customers;
+        public static Semaphore order_ready = new(0, n_customers);
+        public static Semaphore slice_ready = new(0, n_customers);
         
         // Locks for thread safety
         public static object orderLock = new();
         public static object pickUpLock = new();
         public static object workingSurfaceLock = new();
         
-        // Completion tracking
-        private static int completedCustomers = 0;
-        private static object completionLock = new();
+        //do not change any class variable under this line
+        public static LinkedList<PizzaOrder> order = new();
+        public static LinkedList<PizzaDish> pickUp = new();
+        public static LinkedList<PizzaSlice> workingsurface = new();
+
+        public static Pizzaiolo[] pizzaioli = new Pizzaiolo[n_pizzaioli];
+        public static Customer[] customers = new Customer[n_customers];
+        
+        private static Thread[] pizzaioliThreads;
+        private static Thread[] customerThreads;
         
         static void Main(string[] args)
         {
-            if (n_customers % n_slices != 0)
+            if (n_customers % n_slices != 0) //check if n_customers is a multiple of n_slices DO NOT ALTER THIS CODE.
             {
                 throw new Exception("n_customers must be a multiple of n_slices");
             }
+            //init environment variables here if needed
+            pizzaioliThreads = new Thread[n_pizzaioli];
+            customerThreads = new Thread[n_customers];
+            
+            //do not change any code of the following 3 function call. You can add code before and after them
+            //init pizzaioli and customers
+            InitPeople();
+            //activate pizzaioli
+            ActivatePizzaioli();
+            //activate customers
+            ActivateCustomers();
+            // insert code here if necessary
 
-            pizzaioli = new Pizzaiolo[n_pizzaioli];
-            customers = new Customer[n_customers];
+            // Thread.Sleep(100);
             
-            // Create threads
-            Thread[] pizzaioliThreads = new Thread[n_pizzaioli];
-            Thread[] customerThreads = new Thread[n_customers];
-            
-            // Initialize and start pizzaioli
-            Console.WriteLine("Starting pizzaioli...");
-            for (int i = 0; i < n_pizzaioli; i++)
-            {
-                int id = i;
-                pizzaioli[id] = new Pizzaiolo(id);
-                pizzaioliThreads[id] = new Thread(pizzaioli[id].Start);
-                pizzaioliThreads[id].Start();
-            }
-            
-            Thread.Sleep(100);
-            
-            // Initialize and start customers
-            Console.WriteLine("Starting customers...");
-            for (int i = 0; i < n_customers; i++)
-            {
-                int id = i;
-                customers[id] = new Customer(id);
-                customerThreads[id] = new Thread(customers[id].Start);
-                customerThreads[id].Start();
-            }
-
-            // Wait for all customers to finish
             foreach (var thread in customerThreads)
             {
                 thread.Join();
             }
             
-            // Wait for all pizzaioli to finish
             foreach (var thread in pizzaioliThreads)
             {
                 thread.Join();
@@ -84,37 +67,36 @@ namespace pizzeria //this is useless, if you remove it your assignment will be N
             Console.WriteLine($"Order location: There are {order.Count} orders left.");
         }
 
-        // private static void ActivateCustomers() // todo: implement this method
-        // {
-        //     Console.WriteLine("Starting customers...");
-        //     for (int i = 0; i < n_customers; i++)
-        //     {
-        //         int id = i;
-        //         customers[id] = new Customer(id);
-        //         customerThreads[id] = new Thread(customers[id].Start);
-        //         customerThreads[id].Start();
-        //     }
+        private static void ActivateCustomers() // todo: implement this method
+        {
+            for (int i = 0; i < n_customers; i++)
+            {
+                int id = i;
+                customers[id] = new Customer(id);
+                customerThreads[id] = new Thread(customers[id].Start);
+                customerThreads[id].Start();
+            }
+
+
+        }
+        
+        private static void ActivatePizzaioli() //todo: implement this method
+        {
+            for (int i = 0; i < n_pizzaioli; i++)
+            {
+                int id = i;
+                pizzaioli[id] = new Pizzaiolo(id);
+                pizzaioliThreads[id] = new Thread(pizzaioli[id].Start);
+                pizzaioliThreads[id].Start();
+            }
+        
+        }
         //
-        // }
-        //
-        // private static void ActivatePizzaioli() //todo: implement this method
-        // {
-        //     Console.WriteLine("Starting pizzaioli...");
-        //     for (int i = 0; i < n_pizzaioli; i++)
-        //     {
-        //         int id = i;
-        //         pizzaioli[id] = new Pizzaiolo(id);
-        //         pizzaioliThreads[id] = new Thread(pizzaioli[id].Start);
-        //         pizzaioliThreads[id].Start();
-        //     }
-        //
-        // }
-        //
-        // private static void InitPeople()
-        // {
-        //     pizzaioli = new Pizzaiolo[n_pizzaioli];
-        //     customers = new Customer[n_customers];
-        // }
+        private static void InitPeople()
+        {
+            pizzaioli = new Pizzaiolo[n_pizzaioli];
+            customers = new Customer[n_customers];
+        }
         
     }
 
